5fa1b287e0c00efb053bd2c6c02571ff9fde25e5
Kyungjoo Kim <kyukim@sandia.gov>
2021-02-05 13:23:07 -0700

    Tines - copyright

--- src/core/linear-algebra/Tines_Set.hpp
+++ src/core/linear-algebra/Tines_Set.hpp
@@ -1,3 +1,23 @@
+/*----------------------------------------------------------------------------------
+Tines - Time Integrator, Newton and Eigen Solver -  version 1.0
+Copyright (2021) NTESS
+https://github.com/sandialabs/Tines
+
+Copyright 2021 National Technology & Engineering Solutions of Sandia, LLC (NTESS). 
+Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains 
+certain rights in this software.
+
+This file is part of Tines. Tines is open-source software: you can redistribute it
+and/or modify it under the terms of BSD 2-Clause License
+(https://opensource.org/licenses/BSD-2-Clause). A copy of the license is also
+provided under the main directory
+Questions? Kyungjoo Kim <kyukim@sandia.gov>, or
+	   Oscar Diaz-Ibarra at <odiazib@sandia.gov>, or
+	   Cosmin Safta at <csafta@sandia.gov>, or
+	   Habib Najm at <hnnajm@sandia.gov>
+
+Sandia National Laboratories, New Mexico, USA
+----------------------------------------------------------------------------------*/
 #ifndef __TINES_SET_HPP__
 #define __TINES_SET_HPP__
 

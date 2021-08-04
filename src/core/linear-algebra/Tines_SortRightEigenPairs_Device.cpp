1c56ebdd910db380b6754e924c218276749d7dc1
Kyungjoo Kim <kyukim@sandia.gov>
2021-08-04 11:30:48 -0600

    Tines - matching getz

--- src/core/linear-algebra/Tines_SortRightEigenPairs_Device.cpp
+++ src/core/linear-algebra/Tines_SortRightEigenPairs_Device.cpp
@@ -3,8 +3,8 @@ Tines - Time Integrator, Newton and Eigen Solver -  version 1.0
 Copyright (2021) NTESS
 https://github.com/sandialabs/Tines
 
-Copyright 2021 National Technology & Engineering Solutions of Sandia, LLC (NTESS). 
-Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains 
+Copyright 2021 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains
 certain rights in this software.
 
 This file is part of Tines. Tines is open-source software: you can redistribute it
@@ -33,12 +33,12 @@ namespace Tines {
       pair_type * s = (pair_type*)w;
 
       /// set index type
-      for (int i=0;i<m;++i) 
+      for (int i=0;i<m;++i)
         s[i] = pair_type(i, a[i*as]);
 
       /// sort
       std::sort(s, s+m, [](const pair_type &x, const pair_type &y) {
-          return (x.second < y.second);
+          return (x.second > y.second);
         });
 
       /// put it back
@@ -46,12 +46,39 @@ namespace Tines {
         p[i*ps] = s[i].first;
       }
       return 0;
-    }         
+    }
+    template<typename MemberType, typename IntType, typename ValueType>
+    inline static int
+    invoke(const MemberType &member, const int m,
+           const ValueType * ar, const int ars, /// 1d array
+	   const ValueType * ai, const int ais, /// 1d array
+           IntType * p, const int ps,
+           ValueType * w) { /// work space is 2*m
+      using pair_type = std::pair<IntType,ValueType>;
+      pair_type * s = (pair_type*)w;
+
+      /// set index type
+      for (int i=0;i<m;++i) {
+	ValueType mag = std::abs(std::complex<ValueType>(ar[i*ars],ai[i*ais]));
+        s[i] = pair_type(i, mag);
+      }
+
+      /// sort
+      std::sort(s, s+m, [](const pair_type &x, const pair_type &y) {
+          return (x.second > y.second);
+        });
+
+      /// put it back
+      for (int i=0;i<m;++i) {
+        p[i*ps] = s[i].first;
+      }
+      return 0;
+    }
   };
-  
+
 #if defined(KOKKOS_ENABLE_SERIAL)
   int SortRightEigenPairsDevice<Kokkos::Serial>::
-  invoke(const Kokkos::Serial &exec_instance, 
+  invoke(const Kokkos::Serial &exec_instance,
          const value_type_2d_view<double, typename UseThisDevice<Kokkos::Serial>::type> &er,
          const value_type_2d_view<double, typename UseThisDevice<Kokkos::Serial>::type> &ei,
          const value_type_3d_view<double, typename UseThisDevice<Kokkos::Serial>::type> &V,
@@ -61,13 +88,13 @@ namespace Tines {
     const int np = er.extent(0), m = er.extent(1);
     /// W > m * max(3, m)
     {
-      const int workspace = W.span(), workspace_required = m*std::max(3,m); 
-      
+      const int workspace = W.span(), workspace_required = m*std::max(3,m);
+
       TINES_CHECK_ERROR(workspace < workspace_required, "Workspace is smaller than the required m*max(3,m) where m is the dimension of the system");
     }
     double * buf = W.data();
     int * pptr = (int*)buf; buf += m;
-    double * wptr = buf; 
+    double * wptr = buf;
 
     const auto member = Tines::HostSerialTeamMember();
     for (int i = 0; i < np; ++i) {
@@ -78,10 +105,11 @@ namespace Tines {
       double * erptr = _er.data(), * eiptr = _ei.data(), * vptr = _V.data();
       const int ers = _er.stride(0), eis = _ei.stride(0),
         vs0 = _V.stride(0), vs1 = _V.stride(1);
-      
+
       ComputePermutationInternal
         ::invoke(member, m,
                  erptr, ers,
+                 eiptr, eis,
                  pptr, 1,
                  wptr);
 
@@ -98,7 +126,7 @@ namespace Tines {
       CopyInternal
         ::invoke(member, m,
                  eiptr, eis,
-                 wptr, 1);      
+                 wptr, 1);
       ApplyPermutationVectorBackwardInternal
         ::invoke(member, m,
                  pptr, 1,
@@ -115,15 +143,15 @@ namespace Tines {
                  pptr, 1,
                  wptr, 1, m,
                  vptr, vs1, vs0);
-    }      
-    
+    }
+
     Kokkos::Profiling::popRegion();
     return 0;
   }
 #endif
 #if defined(KOKKOS_ENABLE_OPENMP)
   int SortRightEigenPairsDevice<Kokkos::OpenMP>::
-  invoke(const Kokkos::OpenMP &exec_instance, 
+  invoke(const Kokkos::OpenMP &exec_instance,
          const value_type_2d_view<double, typename UseThisDevice<Kokkos::OpenMP>::type> &er,
          const value_type_2d_view<double, typename UseThisDevice<Kokkos::OpenMP>::type> &ei,
          const value_type_3d_view<double, typename UseThisDevice<Kokkos::OpenMP>::type> &V,
@@ -133,7 +161,7 @@ namespace Tines {
     const int np = er.extent(0), m = er.extent(1);
     /// W > m * max(3, m)
     {
-      const int workspace = W.extent(1), workspace_required = m*std::max(3,m); 
+      const int workspace = W.extent(1), workspace_required = m*std::max(3,m);
       TINES_CHECK_ERROR(workspace < workspace_required, "Workspace is smaller than the required m*max(3,m)/per sample where m is the dimension of the system");
     }
 
@@ -147,7 +175,7 @@ namespace Tines {
         const auto _ei = Kokkos::subview(ei, i, Kokkos::ALL());
         const auto _V  = Kokkos::subview(V,  i, Kokkos::ALL(), Kokkos::ALL());
         const auto _W  = Kokkos::subview(W,  i, Kokkos::ALL());
-        
+
         double * erptr = _er.data(), * eiptr = _ei.data(), * vptr = _V.data(), * wptr = _W.data() + m;
         int * pptr = (int*)_W.data();
         const int ers = _er.stride(0), eis = _ei.stride(0), vs0 = _V.stride(0), vs1 = _V.stride(1);
@@ -155,9 +183,10 @@ namespace Tines {
         ComputePermutationInternal
           ::invoke(member, m,
                    erptr, ers,
+		   eiptr, eis,
                    pptr, 1,
                    wptr);
-        
+
         CopyInternal
           ::invoke(member, m,
                    erptr, ers,
@@ -167,17 +196,17 @@ namespace Tines {
                    pptr, 1,
                    wptr, 1,
                    erptr, ers);
-        
+
         CopyInternal
           ::invoke(member, m,
                    eiptr, eis,
-                   wptr, 1);      
+                   wptr, 1);
         ApplyPermutationVectorBackwardInternal
           ::invoke(member, m,
                    pptr, 1,
                    wptr, 1,
                    eiptr, eis);
-        
+
         CopyInternal
           ::invoke(member, Trans::NoTranspose(),
                    m, m,
@@ -189,14 +218,14 @@ namespace Tines {
                    wptr, 1, m,
                    vptr, vs1, vs0);
       });
-    
+
     Kokkos::Profiling::popRegion();
     return 0;
   }
 #endif
 #if defined(KOKKOS_ENABLE_CUDA)
   int SortRightEigenPairsDevice<Kokkos::Cuda>::
-  invoke(const Kokkos::Cuda &exec_instance, 
+  invoke(const Kokkos::Cuda &exec_instance,
          const value_type_2d_view<double, typename UseThisDevice<Kokkos::Cuda>::type> &er,
          const value_type_2d_view<double, typename UseThisDevice<Kokkos::Cuda>::type> &ei,
          const value_type_3d_view<double, typename UseThisDevice<Kokkos::Cuda>::type> &V,
@@ -206,7 +235,7 @@ namespace Tines {
     const int np = er.extent(0), m = er.extent(1);
     /// W > m * max(3, m)
     {
-      const int workspace = W.extent(1), workspace_required = m*std::max(3,m); 
+      const int workspace = W.extent(1), workspace_required = m*std::max(3,m);
       printf("workspace %d, required %d\n", workspace, workspace_required);
       TINES_CHECK_ERROR(workspace < workspace_required, "Workspace is smaller than the required m*max(3,m)/per sample where m is the dimension of the system");
     }
@@ -215,8 +244,9 @@ namespace Tines {
       using host_exec_space = Kokkos::DefaultHostExecutionSpace;
       using host_device_type = typename UseThisDevice<host_exec_space>::type;
       auto er_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), er);
+      auto ei_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), ei);
       auto W_host = Kokkos::create_mirror_view(Kokkos::HostSpace(), W);
-      
+
       using policy_type = Kokkos::TeamPolicy<host_exec_space>;
       policy_type policy(host_exec_space(), np, 1);
       Kokkos::parallel_for
@@ -224,15 +254,17 @@ namespace Tines {
          policy, [=](const typename policy_type::member_type &member) {
           const int i = member.league_rank();
           const auto _er = Kokkos::subview(er_host, i, Kokkos::ALL());
+	  const auto _ei = Kokkos::subview(ei_host, i, Kokkos::ALL());
           const auto _W  = Kokkos::subview(W_host,  i, Kokkos::ALL());
-        
-          double * erptr = _er.data(),* wptr = _W.data() + m;
+
+          double * erptr = _er.data(), * eiptr = _ei.data(), * wptr = _W.data() + m;
           int * pptr = (int*)_W.data();
-          const int ers = _er.stride(0);
-          
+          const int ers = _er.stride(0), eis = _ei.stride(0);
+
           ComputePermutationInternal
             ::invoke(member, m,
                      erptr, ers,
+		     eiptr, eis,
                      pptr, 1,
                      wptr);
         });
@@ -243,7 +275,7 @@ namespace Tines {
     {
       using policy_type = Kokkos::TeamPolicy<Kokkos::Cuda>;
       policy_type policy(exec_instance, np, Kokkos::AUTO);
-      
+
       /// let's guess....
       if (np > 100000) {
         /// we have enough batch parallelism... use AUTO
@@ -274,7 +306,7 @@ namespace Tines {
         policy =
           policy_type(exec_instance, np, team_size, vector_size);
       }
-      
+
       Kokkos::parallel_for
         ("Tines::SortRightEigenPairsCuda::parallel_for",
          policy,  KOKKOS_LAMBDA(const typename policy_type::member_type &member) {
@@ -283,11 +315,11 @@ namespace Tines {
           const auto _ei = Kokkos::subview(ei, i, Kokkos::ALL());
           const auto _V  = Kokkos::subview(V,  i, Kokkos::ALL(), Kokkos::ALL());
           const auto _W  = Kokkos::subview(W,  i, Kokkos::ALL());
-        
+
           double * erptr = _er.data(), * eiptr = _ei.data(), * vptr = _V.data(), * wptr = _W.data() + m;
           int * pptr = (int*)_W.data();
           const int ers = _er.stride(0), eis = _ei.stride(0), vs0 = _V.stride(0), vs1 = _V.stride(1);
-        
+
           CopyInternal
             ::invoke(member, m,
                      erptr, ers,
@@ -297,17 +329,17 @@ namespace Tines {
                      pptr, 1,
                      wptr, 1,
                      erptr, ers);
-          
+
           CopyInternal
             ::invoke(member, m,
                      eiptr, eis,
-                     wptr, 1);      
+                     wptr, 1);
           ApplyPermutationVectorBackwardInternal
             ::invoke(member, m,
                      pptr, 1,
                      wptr, 1,
                      eiptr, eis);
-          
+
           CopyInternal
             ::invoke(member, Trans::NoTranspose(),
                      m, m,
@@ -320,7 +352,7 @@ namespace Tines {
                      vptr, vs1, vs0);
         });
     }
-    
+
     Kokkos::Profiling::popRegion();
     return 0;
   }
